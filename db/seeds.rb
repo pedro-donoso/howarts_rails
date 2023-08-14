b61ffa4df49177d8263ca427861548c83c328ed1
pedro-donoso <pedro.donoso.r@usach.cl>
2023-08-13 23:54:40 -0400

    traigo datos de los campos solicitados desde faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@ puts 'Creando 10 caracteres fake...'
 
 10.times do |i|
     @characters = Character.create(
-                    name: Faker::Avatar.image, 
-                    location: "Imagen #{i}", 
-                    house: "Imagen #{i}"
+                    name: Faker::Movies::HarryPotter.character, 
+                    location: Faker::Movies::HarryPotter.location, 
+                    house: Faker::Movies::HarryPotter.house
                                     )
     print @characters
 end
